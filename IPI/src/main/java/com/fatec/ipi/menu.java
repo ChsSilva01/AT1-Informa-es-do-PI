6b29016f0f9be500ed53867f5a796ee61d427bc8
carloshenrique <chssilva1419@gmail.com>
2025-09-26 19:14:59 -0300

    chssilva1419

--- IPI/src/main/java/com/fatec/ipi/menu.java
+++ IPI/src/main/java/com/fatec/ipi/menu.java
@@ -15,27 +15,42 @@ public class menu {
     apresentacao ap = new apresentacao();
     ods o = new ods();
     integrantes inte = new integrantes();
-    private int verificacao;
     
     public void principal(){
         int navegar, i = 0;
         
         while(i < 1){
-            System.out.println("O que gostaria de fazer? \n 1 - Preencher o texto \n 2 - Escolher a ODS\n 3 - Informar os integrantes\n 4 - Sair");
+            System.out.println("O que gostaria de fazer? \n 1 - Preencher o texto ou exibir \n 2 - Escolher a ODS ou exibir\n 3 - Informar os integrantes ou exibir\n 4 - Sair");
             navegar = read.nextInt();
+            System.out.println("----------------------------------");
             
             switch(navegar){
                 case 1:
-                    ap.cadastrarApresentacao();
-                    ap.exibirApresentar();
+                    if(ap.getVerificacao() == 0){
+                        ap.cadastrarApresentacao();
+                        System.out.println("----------------------------------");
+                    } else if (ap.getVerificacao() != 0){
+                        ap.exibirApresentar();
+                        System.out.println("----------------------------------");
+                    }                    
                     break;
                 case 2:
-                    o.cadastrarODS();
-                    o.exibirODS();
+                    if(o.getVerificacao() == 0){
+                        o.cadastrarODS();
+                        System.out.println("----------------------------------");
+                    } else if (o.getVerificacao() != 0){
+                        o.exibirODS();
+                        System.out.println("----------------------------------");
+                    }
                     break;
-                case 3:                    
-                    inte.cadastrarIntegrantes();
-                    inte.exibirIntegrantes();
+                case 3:
+                    if(inte.getVerificacao() == 0){
+                        inte.cadastrarIntegrantes();
+                        System.out.println("----------------------------------");
+                    } else if (o.getVerificacao() != 0){
+                        inte.exibirIntegrantes();
+                        System.out.println("----------------------------------");
+                    }
                     break;
                 case 4:
                     i++;
